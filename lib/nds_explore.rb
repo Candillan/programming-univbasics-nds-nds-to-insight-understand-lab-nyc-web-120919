764220fb133140fbb49cb17fe450084fa5757fa9
careful-principle-0303 <cabaker96@aol.com>
2019-12-06 21:24:24 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ end
 
 def print_first_directors_movie_titles
   counter = 0 
-  while counter < directors_database[0].length do
+  while counter < directors_database[0][:movies].length do
     puts directors_database[0][:movies][counter][:title]
     counter += 1
   end
